3caf5f567796366b51a54a506a7c854ae292e15f
tetio <tetio@live.com>
2024-12-27 19:15:53 +0100

    rna-transcript done

--- rna-transcription/src/main/java/RnaTranscription.java
+++ rna-transcription/src/main/java/RnaTranscription.java
@@ -1,7 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 class RnaTranscription {
+    private static final Map<Character, Character> map = new HashMap<>();
+
+    static {
+        map.put('G', 'C');
+        map.put('C', 'G');
+        map.put('T', 'A');
+        map.put('A', 'U');
+    }
 
     String transcribe(String dnaStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < dnaStrand.length(); i++) {
+            sb.append(map.get(dnaStrand.charAt(i)));
+        }
+        return sb.toString();
     }
 
 }
