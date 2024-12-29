db4111805dd1a6d364105113e2cea20f6fd8c5e9
tetio <tetio@live.com>
2024-12-29 17:21:12 +0100

    perfect numbers

--- acronym/src/main/java/Acronym.java
+++ acronym/src/main/java/Acronym.java
@@ -1,11 +1,15 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 class Acronym {
+    String acronym;
 
     Acronym(String phrase) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        String aux = phrase.replaceAll("[-_,]", " ");
+        acronym = Arrays.stream(aux.split(" ")).filter(c -> !c.isEmpty()).map(w -> w.substring(0, 1).toUpperCase()).collect(Collectors.joining());
     }
 
     String get() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return acronym;
     }
-
 }
